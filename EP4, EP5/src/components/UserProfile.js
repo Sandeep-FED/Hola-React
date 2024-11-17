84084bf4c3e9215e778cab998c2d8608512d7fa8
Sandeep P S <sandymankara11@gmail.com>
2024-11-17 16:15:46 +0530

    add login, logout conditional rendering

--- EP4, EP5/src/components/UserProfile.js
+++ EP4, EP5/src/components/UserProfile.js
@@ -2,9 +2,27 @@ import React from "react"
 import { User } from "lucide-react"
 
 export const UserProfile = () => {
+
+  const [loggedIn, setLoggedIn] = React.useState(false)
+
   return (
     <div className='user-profile-container'>
-      <User size={16} className='avatar-icon' />
+      {!loggedIn ? (
+        <p
+          onClick={() => setLoggedIn(true)}
+          className='delivery-time avatar-icon'
+        >
+          Login
+        </p>
+      ) : (
+        loggedIn && (
+          <User
+            size={16}
+            className='avatar-icon'
+            onClick={() => setLoggedIn(false)}
+          />
+        )
+      )}
     </div>
   )
 }
