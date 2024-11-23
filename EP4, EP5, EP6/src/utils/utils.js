21299181f8c296fc82fb7136abea9aec0c3212ed
Sandeep P S <sandymankara11@gmail.com>
2024-11-23 13:23:39 +0530

    add util function for displaying veg or non-veg flag

--- EP4, EP5, EP6/src/utils/utils.js
+++ EP4, EP5, EP6/src/utils/utils.js
@@ -1,8 +1,22 @@
+import { Vegan } from "lucide-react"
 import { ShimmerCard } from "../components/ShimmerCard"
 import { SHIMMER_COUNT } from "../constants/common"
+import { Vegan, Egg } from "lucide-react"
 
 export const fillShimmerCards = () => {
   return Array(SHIMMER_COUNT)
     .fill("")
     .map((e, index) => <ShimmerCard key={index} />)
+}
+
+export const IsVegOrNonVeg = (isVeg) => {
+  return isVeg === 1 ? (
+    <div className='veg-container'>
+      <Vegan size={20} className='vegornon-icon' />
+    </div>
+  ) : (
+    <div className='non-veg-container'>
+      <Egg size={20} className='vegornon-icon' />
+    </div>
+  )
 }
