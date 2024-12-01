48427d654dfbef9d83a209d3ef7c27e6256c3391
Sandeep P S <sandymankara11@gmail.com>
2024-12-01 16:26:33 +0530

    implement custom hook for checking online status

--- EP4, EP5, EP6/src/app.js
+++ EP4, EP5, EP6/src/app.js
@@ -1,13 +1,29 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { HeaderComponent } from "./components/Header"
-
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"
 import { Help } from "./components/Help"
 import { Body } from "./components/Body"
 import { RestaurantMenu } from "./components/RestaurantMenu"
+import { About } from "./components/About"
+import { AboutClass } from "./components/AboutClass"
+import "../style.css"
+import { useUserOnlineStatus } from "./hooks/useUserOnlineStatus"
 
 const AppLayout = () => {
+  const isOnline = useUserOnlineStatus()
+
+  if (!isOnline)
+    return (
+      <div className='network-down-wrapper'>
+        <h1>Network Issue</h1>
+        <img
+          src={require("../src/images/undraw_server_down_s4lk.png")}
+          alt='Network issue image illustration'
+        />
+      </div>
+    )
+
   return (
     <div className='app'>
       <HeaderComponent />
@@ -28,6 +44,10 @@ const appRouter = createBrowserRouter([
       {
         path: "/help",
         element: <Help />,
+      },
+      {
+        path: "/about",
+        element: <AboutClass name={"hello"} />,
       },
       {
         path: "/restaurants/:resId",
